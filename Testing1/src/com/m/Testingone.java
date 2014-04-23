fec90c5eb5a1ae0cb5429dd9f89364715171bce7
gitmadhu <madhub2v@gmail.com>
2014-04-23 18:25:44 +0530

    commit second time

--- Testing1/src/com/m/Testingone.java
+++ Testing1/src/com/m/Testingone.java
@@ -7,7 +7,7 @@ public class Testingone {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello testing..");
+		System.out.println("hello testing..second time");
 
 	}
 
